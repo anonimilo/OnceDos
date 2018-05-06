cccca157f35426f01eeeb4ee2d13d1047042aa55
Camilo Restrepo Obando <camiastro6300@gmail.com>
2018-05-06 17:27:48 -0500

    Avance

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-const express= require('express');
-const app=express();
 
-app.get('/personas',function(req,res){
-    app.send(
-"Hola"
-    )
+const MongoClient= require('mongodb').MongoClient,
+express=require('express'),
+engines=require('consolidate');
+handle = require('handlebars');
+var app=express(),
+db;
+
+app.engine('hbs',engines.handlebars);
+
+app.set('views','./views');
+app.set('view engine','hbs');
+
+MongoClient.connect('mongodb://localhost:27017',function(err,client){
+if(err) throw
+
+db=client.db('test');
+app.listen(1234);
+
 });
-app.listen(5000);
+
+
+app.get('/',(req,res)=>{
+
+
+var prod = db.collection('conos')
+.find()
+
+prod.toArray((err,result)=>{
+    console.log('holiwis')
+res.render('index',{
+productos:result
+
+});
+
+})
+
+
+});
